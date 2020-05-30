e075dad82945a7cc6261e84299532bc8403e95d1
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-05-30 17:39:04 -0400

    excel utility class

--- src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
+++ src/test/java/com/bookit/step_definitions/APIStepDefinitions.java
@@ -9,11 +9,13 @@ import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.MatcherAssert;
 import org.junit.Assert;
 
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefinitions {
     private RequestSpecification requestSpecification; //this is what we put in given
@@ -68,5 +70,6 @@ public class APIStepDefinitions {
     public void user_payload_contains_following_room_names(List<String> dataTable) {
         List<String> roomNames = response.jsonPath().getList("name");
         Assert.assertTrue(roomNames.containsAll(dataTable));
+        MatcherAssert.assertThat(roomNames, hasItem(in(dataTable)));
     }
 }
