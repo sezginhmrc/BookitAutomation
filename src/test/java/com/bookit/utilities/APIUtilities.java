6ea75a9f783a743d67d79018cdfdc58829f32d53
sezginhmrc <sezginhamurcu@icloud.com>
2020-05-30 16:22:22 -0400

    Api

--- src/test/java/com/bookit/utilities/APIUtilities.java
+++ src/test/java/com/bookit/utilities/APIUtilities.java
@@ -1,4 +1,66 @@
 package com.bookit.utilities;
 
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.*;
 public class APIUtilities {
+
+
+
+    static {
+        baseURI = Environment.BASE_URI;
+    }
+    /*
+    This Api Utilities class including handy methods for api testing
+     * This method is issued to retieve token from the server.
+     * Token must be attached to te header of every api call.
+     * @return
+     */
+    public static String getToken(){
+        Response response = given().
+                                    queryParam("email",Environment.LEADER_USERNAME).
+                                    queryParam("password",Environment.LEADER_PASSWORD).
+                            when().
+                                    get("/sign");
+
+        response.then().log().ifError(); // if request failed print response info
+
+        String token = response.jsonPath().getString("accessToken");
+        System.out.println("TOKEN :: "+token);
+        return token ;
+    }
+
+   /*
+    This Api Utilities class including handy methods for api testing
+     * This method is issued to retieve token from the server.
+     * Token must be attached to te header of every api call.
+     * @return
+     */
+
+    public static String getToken(String role){
+        // We have 3 rule in the application -> leader,memeber,teacher.
+        String email = null;
+        String password =null;
+        if(role.toLowerCase().contains("teacher")){
+            email = Environment.TEACHER_USERNAME;
+            password = Environment.TEACHER_PASSWORD;
+        } else if(role.toLowerCase().contains("lead")){
+            email = Environment.LEADER_USERNAME;
+            password = Environment.LEADER_PASSWORD;
+        } else {
+            email = Environment.MEMBER_USERNAME; // student
+            password = Environment.MEMBER_PASSWORD;
+        }
+
+        Response response = given().
+                queryParam("email",email).
+                queryParam("password",password).
+                when().
+                get("/sign");
+
+        response.then().log().ifError(); // if request failed print response info
+
+        String token = response.jsonPath().getString("accessToken");
+        System.out.println("TOKEN :: "+token);
+        return token ;
+    }
 }
