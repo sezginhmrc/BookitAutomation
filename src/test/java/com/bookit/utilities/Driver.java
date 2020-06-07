00987c882d8c09d56dfa0907ce600c7fdbe742d9
sezginhmrc <sezginhamurcu@icloud.com>
2020-06-07 13:52:03 -0400

    firefoxremotefixed

--- src/test/java/com/bookit/utilities/Driver.java
+++ src/test/java/com/bookit/utilities/Driver.java
@@ -31,7 +31,7 @@ public class Driver {
      * @return
      */
     public synchronized static WebDriver getDriver() {
-        String GRID_URL = "http://35.171.158.59:4444/wd/hub";
+        String GRID_URL = "http://34.204.195.171:4444/wd/hub";
         //if webdriver object doesn't exist
         //create it
         if (driverPool.get() == null) {
